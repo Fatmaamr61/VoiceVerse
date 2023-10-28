9f4312b20f534e1997da0bd8ac9622bfa894a8ba
Fatma Amr <fatmaamr@Fatmas-MacBook-Pro.local>
2023-10-28 16:52:42 +0200

    final fixing cors

--- src/app.router.js
+++ src/app.router.js
@@ -15,7 +15,7 @@ export const appRouter = (app, express) => {
   app.use("/auth", authRouter);
 
   // CORS
-  const whitelist = ["http://127.0.0.1:5500"];
+  const whitelist = ["http://127.0.0.1:3000"];
 
   app.use((req, res, next) => {
     console.log(req.header("origin"));
