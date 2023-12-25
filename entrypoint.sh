83cc8c1bfc9d3771d63ef4e9f44efc968bc91c42
Abdel-RahmanSaied <abdo.am169@gmail.com>
2023-12-25 22:45:12 +0300

    refactor the docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 # This line specifies that the script should be interpreted using Bash
 
 sudo apt-get install portaudio19-dev
+#sudo apt-get install portaudio19-dev
 # This line installs the 'portaudio19-dev' package using the package manager 'apt-get'.
 # The 'sudo' command is used to run the installation with elevated privileges.
