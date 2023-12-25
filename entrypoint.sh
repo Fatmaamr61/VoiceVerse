59b545cee97cb564bb19b64e03cb99d2ba3bad6c
Fatma Amr <fatmaamr@Fatmas-MacBook-Pro.local>
2023-12-26 00:01:49 +0200

    fetching

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This line specifies that the script should be interpreted using Bash
-
-sudo apt-get install portaudio19-dev
-#sudo apt-get install portaudio19-dev
-# This line installs the 'portaudio19-dev' package using the package manager 'apt-get'.
-# The 'sudo' command is used to run the installation with elevated privileges.
--- src/modules/translating/translator.controller.js
+++ src/modules/translating/translator.controller.js
@@ -1,6 +1,28 @@
+import fetch from "node-fetch";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
-export const getURL = asyncHandler (async(req, res, next ) => {
-    const url = req.body;
-    
-})
+export const getURL = asyncHandler(async (req, res, next) => {
+  const { url } = req.body;
+  const translator_response = await fetch(
+    "http://flask-container:6000/translate",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        audio_url: url,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  if (!translator_response.ok) {
+    // Handle non-successful status code
+    console.log("trans:  ", translator_response.json());
+    throw new Error(`HTTP error! Status: ${translator_response.status}`);
+  }
+
+  const data = await translator_response.json();
+
+  console.log(data);
+  return res.json(data);
+});
