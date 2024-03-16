61d8945ae1f11d1fe110469e0f8679c0b087d3d7
Fatma Amr <fatmaamr@Fatmas-MacBook-Pro.local>
2024-03-16 16:23:59 +0200

    fixings

--- src/app.router.js
+++ src/app.router.js
@@ -18,7 +18,7 @@ export const appRouter = (app, express) => {
 
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
 
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "token"],
   };
 
   app.use(cors(corsOpts));
